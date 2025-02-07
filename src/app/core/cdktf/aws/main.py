6e45c1a7f0dadfd7044e3e05e2693a13e7b0db35
Nareshp1 <nareshp0331@yahoo.com>
2025-02-07 03:25:24 -0500

    Added functionality to automate the deployment of aws infra and then delete files after deployment from local system with the plan tos store in the database.

--- src/app/core/cdktf/aws/main.py
+++ src/app/core/cdktf/aws/main.py
@@ -2,6 +2,9 @@
 import os
 import subprocess
 import tempfile
+import shutil
+from time import sleep
+from pathlib import Path
 
 
 from cdktf import App
@@ -10,21 +13,60 @@ from ....schemas.openlabs import OpenLabsRange
 from .aws_stack import AWSStack
 
 
-def deploy_infrastructure(dir: str) -> None:
-    """Runs `terraform deploy --auto-approve` programmatically."""  # noqa: D401
-    # Change directory to `cdktf.out`
-    os.chdir(dir)
+def deploy_infrastructure(dir: str, stack_name: str) -> str:
+    """Runs `terraform deploy --auto-approve` programmatically.
+    
+    Args:
+    ----
+        dir (str): Output directory.
+        
+    Returns:
+    -------
+        str: Content of the terraform state file.
+
+    """
+    inital_dir = os.getcwd()
+    print(inital_dir)
+    
+    # Change to directory with `cdk.tf.json`
+    synth_output_dir = Path(f"{dir}/stacks/{stack_name}")
+    os.chdir(synth_output_dir)
     print(os.getcwd())
+    state_file = synth_output_dir / Path(f"terraform.{stack_name}.tfstate")
 
     # Run Terraform commands
     print("Running terraform init...")
     subprocess.run(["terraform", "init"], check=True)
 
     print("Running terraform apply...")
-    subprocess.run(["terraform", "apply", "--auto-approve"], check=True)
-
+    subprocess.run(["terraform", "apply", "--auto-approve", f"-state={str(state_file)}"], check=True)
     print("Terraform apply complete!")
 
+    # Read state file into string
+    content = ""
+    with open(state_file, "r", encoding="utf-8") as file:
+        content = file.read()
+
+    # Remove terraform build files
+    os.chdir(inital_dir)
+    shutil.rmtree(dir)
+    
+    return content
+
+# def destroy_infrastructure(cyber_range: OpenLabsRange, state: str) -> bool: # TODO: 
+#     """Destroy terraform infrastructure.
+#    
+#     Args:
+#     ----
+#       cyber_range (OpenLabsRange): Range to delete.
+#       state (str): State file content.
+# 
+#     Returns:
+#     -------
+#       bool: True if successfully destroyed. False otherwise.       
+# 
+#     """
+#     subprocess.run(["terraform", "destroy", "--auto-approve", f"-state={str(state_file_path)}"], check=True)
 
 def create_aws_stack(cyber_range: OpenLabsRange) -> str:
     """Create and synthesize an AWS stack using the provided OpenLabsRange.
@@ -41,8 +83,8 @@ def create_aws_stack(cyber_range: OpenLabsRange) -> str:
     # /tmp/.openlabs-XX/<range uuid>
     tmp_dir = tempfile.mkdtemp(prefix=".openlabs-")
 
-    app = App()
-    AWSStack(app, "aws", cyber_range)
+    app = App(outdir=tmp_dir)
+    AWSStack(app, cyber_range.name, cyber_range)
 
     app.synth()
 
