35056aae3335387fce59903420884adf3a96c2f9
Adamkadaban <adamkadaban@gmail.com>
2025-02-08 01:22:36 -0500

    clean code up and add missing typing

--- src/app/main.py
+++ src/app/main.py
@@ -1,8 +1,9 @@
-import yaml
 import json
 
-from fastapi import Request, HTTPException
+import yaml
+from fastapi import HTTPException, Request, Response, status
 from starlette.datastructures import MutableHeaders
+from starlette.middleware.base import RequestResponseEndpoint
 
 from .api import router
 from .core.config import settings
@@ -12,17 +13,19 @@ app = create_application(router=router, settings=settings)
 
 
 @app.middleware("http")
-async def convert_yaml_to_json(request: Request, call_next):
+async def convert_yaml_to_json(
+    request: Request, call_next: RequestResponseEndpoint
+) -> Response:
+    """FastAPI middleware that will change all requests with a yaml content type to json."""
     body = await request.body()
     if request.headers.get("content-type") == "application/yaml":
         try:
             json_body = json.dumps(yaml.safe_load(body.decode("utf-8"))).encode()
         except Exception as e:
-            print(str(e))
             raise HTTPException(
                 status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-                detail=f"Unable to parse provided YAML configuration.",
-            )
+                detail="Unable to parse provided YAML configuration.",
+            ) from e
 
         request._body = json_body
         updated_headers = MutableHeaders(request._headers)
@@ -30,5 +33,5 @@ async def convert_yaml_to_json(request: Request, call_next):
         updated_headers["content-type"] = "application/json"
         request._headers = updated_headers
         request.scope.update(headers=request.headers.raw)
-    response = await call_next(request)
-    return response
+
+    return await call_next(request)
