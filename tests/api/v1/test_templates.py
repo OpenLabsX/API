67620c476b849cfeffefed683d70f9996255e1d3
Alex Christy <a.christy@ufl.edu>
2025-02-16 12:13:16 -0500

    Update old endpoint tests map to new endpoints

--- tests/api/v1/test_templates.py
+++ tests/api/v1/test_templates.py
@@ -5,11 +5,7 @@ from typing import Any
 from fastapi import status
 from fastapi.testclient import TestClient
 
-from src.app.main import app
-
 from .config import BASE_ROUTE
-
-client = TestClient(app)
 
 ###### Test /template/range #######
 
@@ -55,61 +51,63 @@ def test_template_range_valid_payload(client: TestClient) -> None:
     assert str(uuid_obj) == uuid_response
 
 
-def test_template_range_invalid_vpc_cidr() -> None:
+def test_template_range_invalid_vpc_cidr(client: TestClient) -> None:
     """Test for 422 response when VPC CIDR is invalid."""
     # Use deepcopy to ensure all nested dicts are copied
     invalid_payload = copy.deepcopy(valid_range_payload)
-    invalid_payload["vpc"]["cidr"] = "192.168.300.0/24"  # Assign the invalid CIDR block
-    response = client.post(f"{BASE_ROUTE}/templates/range", json=invalid_payload)
+    invalid_payload["vpcs"][0][
+        "cidr"
+    ] = "192.168.300.0/24"  # Assign the invalid CIDR block
+    response = client.post(f"{BASE_ROUTE}/templates/ranges", json=invalid_payload)
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
 
 
-def test_template_range_invalid_subnet_cidr() -> None:
+def test_template_range_invalid_subnet_cidr(client: TestClient) -> None:
     """Test for 422 response when subnet CIDR is invalid."""
     # Use deepcopy to ensure all nested dicts are copied
     invalid_payload = copy.deepcopy(valid_range_payload)
-    invalid_payload["vpc"]["subnets"][
-        0
+    invalid_payload["vpcs"][0]["subnets"][0][
+        "cidr"
     ] = "192.168.300.0/24"  # Assign the invalid CIDR block
-    response = client.post(f"{BASE_ROUTE}/templates/range", json=invalid_payload)
+    response = client.post(f"{BASE_ROUTE}/templates/ranges", json=invalid_payload)
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
 
 
-def test_template_range_invalid_vpc_subnet_cidr_contain() -> None:
+def test_template_range_invalid_vpc_subnet_cidr_contain(client: TestClient) -> None:
     """Test for 422 response when subnet CIDR is not contained in the VPC CIDR."""
     invalid_payload = copy.deepcopy(valid_range_payload)
 
     # VPC CIDR
-    invalid_payload["vpc"]["cidr"] = "192.168.0.0/16"
+    invalid_payload["vpcs"][0]["cidr"] = "192.168.0.0/16"
 
     # Subnet CIDR
-    invalid_payload["vpc"]["subnets"][0][
+    invalid_payload["vpcs"][0]["subnets"][0][
         "cidr"
     ] = "172.16.1.0/24"  # Assign the invalid CIDR block
 
-    response = client.post(f"{BASE_ROUTE}/templates/range", json=invalid_payload)
+    response = client.post(f"{BASE_ROUTE}/templates/ranges", json=invalid_payload)
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
 
 
-def test_template_range_empty_tag() -> None:
+def test_template_range_empty_tag(client: TestClient) -> None:
     """Test for a 422 response when a tag is empty."""
     invalid_payload = copy.deepcopy(valid_range_payload)
-    invalid_payload["vpc"]["subnets"][0]["hosts"][0]["tags"].append("")
-    response = client.post(f"{BASE_ROUTE}/templates/range", json=invalid_payload)
+    invalid_payload["vpcs"][0]["subnets"][0]["hosts"][0]["tags"].append("")
+    response = client.post(f"{BASE_ROUTE}/templates/ranges", json=invalid_payload)
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
 
 
-def test_template_range_invalid_provider() -> None:
+def test_template_range_invalid_provider(client: TestClient) -> None:
     """Test for a 422 response when the provider is invalid."""
     invalid_payload = copy.deepcopy(valid_range_payload)
     invalid_payload["provider"] = "invalid_provider"  # Not a valid enum value
-    response = client.post(f"{BASE_ROUTE}/templates/range", json=invalid_payload)
+    response = client.post(f"{BASE_ROUTE}/templates/ranges", json=invalid_payload)
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
 
 
-def test_template_range_invalid_hostname() -> None:
+def test_template_range_invalid_hostname(client: TestClient) -> None:
     """Test for a 422 response when a hostname is invalid."""
     invalid_payload = copy.deepcopy(valid_range_payload)
-    invalid_payload["vpc"]["subnets"][0]["hosts"][0]["hostname"] = "-i-am-invalid"
-    response = client.post(f"{BASE_ROUTE}/templates/range", json=invalid_payload)
+    invalid_payload["vpcs"][0]["subnets"][0]["hosts"][0]["hostname"] = "-i-am-invalid"
+    response = client.post(f"{BASE_ROUTE}/templates/ranges", json=invalid_payload)
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
