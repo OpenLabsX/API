a889938d356b43872863d320e77feb5b08b36914
Adamkadaban <adamkadaban@gmail.com>
2025-02-23 17:46:46 -0500

    add user secret and model

---
+++ src/app/models/user_model.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from sqlalchemy import Boolean, String, DateTime
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from ..core.db.database import Base
+from .openlabs_base_model import OpenLabsUserMixin
+
+class OpenLabsUserModel(Base, OpenLabsUserMixin):
+    """SQLAlchemy ORM model for User."""
+
+    __tablename__ = "users"
+
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    email: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    hashed_password: Mapped[str] = mapped_column(String, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    last_active: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+
+    is_admin: Mapped[bool] = mapped_column(Boolean, default=False)
+
+    # One-to-one relationship with secrets. 
+    # For now, users can only have one azure or one aws secret
+    secrets = relationship("OpenLabsSecretModel", back_populates="user", cascade="all, delete-orphan", uselist=False)
---
+++ src/app/schemas/user_schema.py
@@ -0,0 +1,93 @@
+import uuid
+iport datetime
+
+from pydantic import BaseModel, Field, field_validator, ConfigDict
+from email_validator import validate_email, EmailNotValidError
+
+class UserBaseSchema(BaseModel):
+    """Schema for user authentication."""
+
+    email: str = Field(
+        ...,
+        description="Email of user",
+        min_length=1,
+        examples=["adam@ufsit.club", "alex@christy.com", "naresh@panch.al"],
+    )
+
+    password: str = Field(
+        ...,
+        description = "Password of user",
+        min_length = 1,
+        examples=["password123"]
+    )
+
+    @field_validator("email")
+    @classmethod
+    def validate_email(
+        cls, email: str) -> str:
+        """Check that email format is valid.
+        Args:
+        ----
+            cls: OpenLabsUser object.
+            email (str): User email address.
+        Returns:
+        -------
+            str: User email address.
+        """
+
+        try:
+            emailinfo = validate_email(email, check_deliverability=False)
+
+            return emailinfo.normalized
+        except EmailNotValidError:
+            msg = "Provided email address is invalid."
+            raise ValueError(msg)
+
+class UserCreateBaseSchema(UserBaseSchema):
+    """User object for user creation in OpenLabs."""
+
+    name: str = Field(
+        ...,
+        description="Full name of user",
+        min_length=1,
+        examples=["Adam Hassan", "Alex Christy", "Naresh Panchal"]
+    )
+
+    @field_validator("email")
+    @classmethod
+    def validate_email(
+        cls, email: str) -> str:
+        """Check that email format is valid.
+        Args:
+        ----
+            cls: OpenLabsUser object.
+            email (str): User email address.
+        Returns:
+        -------
+            str: User email address.
+        """
+
+        try:
+            # Makes a DNS query to validate deliverability
+            # We do this, as users will only be added to DB on registration
+            emailinfo = validate_email(email, check_deliverability=True)
+
+            return emailinfo.normalized
+        except EmailNotValidError:
+            msg = "Provided email address is invalid."
+            raise ValueError(msg)
+
+
+class UserID(BaseModel):
+    """Identity class for UserCreate."""
+
+    id: uuid.UUID = Field(
+        default_factory=uuid.uuid4, description="Unique user identifier."
+    )
+
+    model_config = ConfigDict(from_attributes=True)
+
+class UserCreateSchema(UserCreateBaseSchema, UserID):
+    """User creation object for OpenLabs."""
+
+    model_config = ConfigDict(from_attributes=True)
